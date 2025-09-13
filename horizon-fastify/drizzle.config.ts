6f633a915bd8a1dfd84e7f3318180d7ce7e3cd61
JB Lee <intellijb@gmail.com>
2025-09-13 11:17:16 +0900

    fix(fastify): drizzle env

--- horizon-fastify/drizzle.config.ts
+++ horizon-fastify/drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'drizzle-kit';
-import 'dotenv/config';
+import { config } from './src/config/index';
 
 export default defineConfig({
   schema: './src/db/schema/index.ts',
   out: './src/db/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URI!,
+    url: config.POSTGRES_URI,
   },
   verbose: true,
   strict: true,
